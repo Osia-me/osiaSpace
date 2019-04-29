728a07f7cabb13e913036779cf04f84418cd44e1
Osia-me <olga.melnykovych@gmail.com>
2019-04-29 16:38:01 +0200

    updated with node server

---
+++ app.js
@@ -0,0 +1,17 @@
+const express     = require('express'),
+      favicon     = require('express-favicon'),
+      app         = express(),
+
+      port        = process.env.PORT || 3000;
+
+//Set-up ejs
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + '/public'));
+app.use(favicon(__dirname + '/public/img/space.png'));
+//Route
+app.get('/', (req,res) => {
+  res.render('index.ejs')
+});
+
+//Server
+app.listen(port, () => console.log(`Server running on port: ${port}`));
